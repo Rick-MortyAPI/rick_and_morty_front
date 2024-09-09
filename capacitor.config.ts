dcfc49717db0fb78279abda0f7b55414bf773bdf
juanpablotr14 <infobethlemitastascon@gmail.com>
2024-09-09 12:25:00 -0500

    Update icon and splash-sreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'MyApplicationTabs',
+  appName: 'Rick&Morty',
   webDir: 'www'
 };
 
