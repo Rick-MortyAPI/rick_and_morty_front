e5436392a0dd96e33e9d158bfb8ba2221ec2d9ad
juanpablotr14 <infobethlemitastascon@gmail.com>
2024-09-09 11:38:43 -0500

    Ajustando ultimos detalles

--- src/app/components/lugares/lugares.component.ts
+++ src/app/components/lugares/lugares.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LocationsServiceService } from '../../services/locations-service.service';
 import { CharactersServiceService } from 'src/app/services/characters-service.service';
-import { InfiniteScrollCustomEvent } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, ModalController } from '@ionic/angular';
+import { PersonajeModalComponent } from '../personaje-modal/personaje-modal.component';
+import { FavoritesServiceService } from 'src/app/services/favorites-service.service';
 
 @Component({
   selector: 'app-lugares',
@@ -10,21 +12,24 @@ import { InfiniteScrollCustomEvent } from '@ionic/angular';
 })
 export class LugaresComponent implements OnInit {
 
-  lugaresList: any[] = []; // Lista de lugares
-  residentesList: { [key: string]: any[] } = {}; // Diccionario para los residentes por lugar
-  loading: boolean = true; // Estado de carga
+  lugaresList: any[] = [];
+  residentesList: { [key: string]: any[] } = {}; 
+  loading: boolean = true;
   currentPage: number = 1;
+  isToastOpen: boolean = false;
+  toastMessage: string = '';
 
   constructor(
     private _locationService: LocationsServiceService,
-    private _rickyMortyService: CharactersServiceService
+    private _rickyMortyService: CharactersServiceService,
+    private modalController: ModalController,
+    private favoritesService: FavoritesServiceService
   ) {}
 
   ngOnInit() {
-    this.getAllLocations(); // Obtener todas las localizaciones al iniciar el componente
+    this.getAllLocations();
   }
 
-  // Obtener todas las localizaciones
   getAllLocations() {
     this._locationService.getLocationsByPage(this.currentPage).subscribe(
       data => {
@@ -41,16 +46,16 @@ export class LugaresComponent implements OnInit {
     );
   }
 
-  // Obtener los residentes de cada lugar
+
   obtenerResidentes(lugar: any) {
     const residents = lugar.residents;
-    this.residentesList[lugar.id] = []; // Inicializar la lista de residentes para el lugar
+    this.residentesList[lugar.id] = []; 
 
     residents.forEach((url: string) => {
       this._rickyMortyService.getCharacterByUrl(url)
         .subscribe(
           data => {
-            this.residentesList[lugar.id].push(data); // Añadir el residente a la lista
+            this.residentesList[lugar.id].push(data);
           },
           error => {
             console.error(`Error fetching resident from ${url}:`, error);
@@ -59,10 +64,42 @@ export class LugaresComponent implements OnInit {
     });
   }
 
+  async openCharacterModal(character: any) {
+    const modal = await this.modalController.create({
+      component: PersonajeModalComponent,
+      componentProps: {
+        character: character
+      }
+    });
+    return await modal.present();
+  }
+
+  isFavorite(character: any): boolean {
+    return this.favoritesService.isFavorite(character);
+  }
+
+  toggleFavorite(character: any) {
+
+    if (this.isFavorite(character)) {
+      this.favoritesService.removeFavorite(character);
+      this.toastMessage = `${character.name} removed from favorites`;
+    } else {
+      this.favoritesService.addFavorite(character);
+      this.toastMessage = `${character.name} added to favorites`;
+    }
+
+    this.isToastOpen = true;
+
+    setTimeout(() => {
+      this.isToastOpen = false;
+    }, 3000);
+
+  }
+
   onIonInfinite(event: InfiniteScrollCustomEvent) {
-    this.currentPage++; // Incrementar el número de página para la siguiente carga
+    this.currentPage++;
     this.getAllLocations();
-    event.target.complete(); // Marcar el infinite scroll como completado
+    event.target.complete();
   }
   
 }
